30c9b1cac1e0175dd7933566ee5a723efd70980d
oooksan <89106064+oooksan@users.noreply.github.com>
2021-08-18 23:18:34 +0300

    traffic light create

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.black
+        startButton.backgroundColor = UIColor.white
+        startButton.setTitleColor(.black, for: .normal)
+        startButton.layer.cornerRadius = 4
+        redView.backgroundColor = UIColor.red
+        orangeView.backgroundColor = UIColor.orange
+        greenView.backgroundColor = UIColor.green
+        
+       
+        redView.alpha = 0.3
+        orangeView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.height/2
+        orangeView.layer.cornerRadius = orangeView.frame.height/2
+        greenView.layer.cornerRadius = greenView.frame.height/2
     }
 
-
+    @IBAction func changeLightButton(_ sender: Any) {
+        if redView.alpha != 1 && orangeView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.3
+            orangeView.alpha = 1
+        } else if orangeView.alpha == 1 {
+            orangeView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+    }
+    
 }
 
